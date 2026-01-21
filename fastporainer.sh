7aedd9f34dafa6bd90f48f9f1b40fc40eac308b2
Egor Smirnov <smirnowegor@gmail.com>
2026-01-21 23:44:01 +0300

    Update Portainer image to latest version

--- fastporainer.sh
+++ fastporainer.sh
@@ -58,7 +58,7 @@ version: '3.8'
 services:
   portainer:
     container_name: portainer
-    image: portainer/portainer-ce:2.31.3
+    image: portainer/portainer-ce:latest
     command: -H unix:///var/run/docker.sock
     restart: always
     ports:
